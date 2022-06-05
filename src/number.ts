c0ee7608d58401bbc8d754f5839258d253fd6373
DeprecatedLuxas <lucasnrgaard@gmail.com>
2022-06-05 10:26:49 +0200

    added positive and negative to numbers

--- src/number.ts
+++ src/number.ts
@@ -8,3 +8,21 @@
 export function clamp(value: number, min: number, max: number): number {
   return Math.min(Math.max(value, min), max);
 }
+
+/**
+ * Test a number to see if it is a negative
+ * @param {number} value the number to test
+ * @returns {boolean} either true or false
+ */
+export function negative(value: number): boolean {
+  return value < 0;
+}
+
+/**
+ * Test a number to see if it is a positive
+ * @param {number} value the number to test
+ * @returns {boolean} either true or false
+ */
+export function positive(value: number): boolean {
+  return value > 0;
+}
--- test/number.test.ts
+++ test/number.test.ts
@@ -1,9 +1,25 @@
 import { describe, expect, it } from "vitest";
-import { clamp } from "../src";
+import { clamp, negative, positive } from "../src";
 
 describe("clamp test", () => {
   it("should return 230 & 100", () => {
     expect(clamp(250, 100, 230)).toEqual(230);
     expect(clamp(50, 100, 230)).toEqual(100);
+  });
+});
+
+describe("negative", () => {
+  it("should return [true, false, false]", () => {
+    expect(negative(-1)).toEqual(true);
+    expect(negative(0)).toEqual(false);
+    expect(negative(1)).toEqual(false);
+  });
+});
+
+describe("positive", () => {
+  it("should return [true, false, true]", () => {
+    expect(positive(999999999)).toEqual(true);
+    expect(positive(0)).toEqual(false);
+    expect(positive(1)).toEqual(true);
   });
 });
