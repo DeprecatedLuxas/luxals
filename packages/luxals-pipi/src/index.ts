5e720cda5e77bfaa87a1c948cc3e4339ba1035d2
DeprecatedLuxas <lucasnrgaard@gmail.com>
2022-06-28 16:42:28 +0200

    fix(pipi): pipi works as intended

--- packages/luxals-pipi/src/index.ts
+++ packages/luxals-pipi/src/index.ts
@@ -1,3 +1,17 @@
-export function pipi(str: string, views: Record<string, string>): string {
-  return "pipi";
+/**
+ * Generate a string from placeholders
+ * @param {String} str The string to replace placeholders in
+ * @param views
+ * @returns
+ */
+export function pipi(str: string, placeholders: PipiPlaceholders): string {
+  return str.replace(/\{([^}]+)\}/g, (_, key) => {
+    const value = placeholders[key];
+    if (typeof value === "function") {
+      return value();
+    }
+    return value;
+  });
 }
+
+export type PipiPlaceholders = Record<string, string | (() => string)>;
--- packages/luxals-pipi/test/pipi.test.ts
+++ packages/luxals-pipi/test/pipi.test.ts
@@ -2,9 +2,15 @@ import { describe, expect, it } from "vitest";
 import { pipi } from "../src";
 
 describe("pipi test", () => {
-  it("should return luxals is cool", () => {
+  it("placeholder with string", () => {
     const str = "luxals is {word}";
     const views = { word: "cool" };
+    expect(pipi(str, views)).toEqual("luxals is cool");
+  });
+
+  it("placeholder with function", () => {
+    const str = "luxals is {word}";
+    const views = { word: () => "cool" };
     expect(pipi(str, views)).toEqual("luxals is cool");
   });
 });
