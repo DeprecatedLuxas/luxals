265e30d2123ee6f3d7bab3c0ba68c8fa2c395cc7
DeprecatedLuxas <lucasnrgaard@gmail.com>
2022-06-29 09:09:37 +0200

    chore: package.json

---
+++ packages/luxals/src/index.ts
@@ -0,0 +1,5 @@
+export * from "@luxass/luxals-number";
+export * from "@luxass/luxals-random";
+export * from "@luxass/luxals-string";
+export * from "@luxass/luxals-pipi";
+export * from "@luxass/luxals-types";
