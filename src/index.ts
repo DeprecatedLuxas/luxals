2688318cf7f834740ee96cdcd123c7145a6283f1
luxass <lucasnrgaard@gmail.com>
2022-07-07 16:45:07 +0200

    feat: mergeWith & setWith

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { get, has, merge, set } from "lodash-es";
+import { get, has, merge, mergeWith, set, setWith } from "lodash-es";
 
-export { get, has, merge, set };
+export { get, has, merge, set, mergeWith, setWith };
 export * from "./number";
 export * from "./random";
 export * from "./string";
