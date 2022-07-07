e5634912741ff19c82f727a61b7a6d9d3e8b04ce
luxass <lucasnrgaard@gmail.com>
2022-07-07 15:51:42 +0200

    fix: issues with named exports?

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { get, has, merge, set } from "lodash";
+
+export { get, has, merge, set };
 export * from "./number";
 export * from "./random";
 export * from "./string";
 export * from "./pipi";
 export * from "./types";
 export * from "fast-memoize";
-export { get, has, merge, set } from "lodash";
